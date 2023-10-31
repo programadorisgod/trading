720fd4fe26961c4b8b038601d0a5785a18ec6504
CamiloDev <catapiascastro@gmail.com>
2023-10-31 13:14:56 -0500

    -mfix(api cais listas)

--- src/Data/Micro/readMicroEconomicas.py
+++ src/Data/Micro/readMicroEconomicas.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os 
 import requests
-
+from datetime import datetime, timedelta
 
 def ipc_anual(filepath='./Datos/1.2.5.IPC_Serie_variaciones.xlsx'):
     df = pd.read_excel(filepath, sheet_name='Sheet1', index_col='Año(aaaa)-Mes(mm)')
@@ -27,17 +27,21 @@ def ipc_anual(filepath='./Datos/1.2.5.IPC_Serie_variaciones.xlsx'):
 
 
 def metales_preciosos(filepath='./Datos/metalespreciosos.xlsx'):
-    df = pd.read_excel(filepath, sheet_name='Sheet1', index_col='Fecha (dd/mm/aaaa)')
+    df = pd.read_excel(filepath, sheet_name='Sheet1')
+ 
     data ={}
-
+    start_date = datetime(2023, 1, 1)
+    current_date =start_date
+    
     for idx, row in df.iterrows():
-        date = str(idx)[:10]
+        date=current_date.strftime('%Y-%m-%d')
         gold={'compra': row['Compra Oro'], 'venta': row['Venta Oro']}
         silver={'compra': row['Compra Plata'], 'venta': row['Venta Plata']}
         platinum={'compra': row['Compra Platino'], 'venta': row['Venta Platino']}
         data[date]={'oro':gold, 'plata':silver,'platino': platinum}
-    
-  
+        print(current_date)
+        current_date += timedelta(days=1)
+
     return data
 
 
