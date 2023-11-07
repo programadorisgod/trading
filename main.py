1d46e8989812d6142d9c740f6ce5f29daf8be54a
CamiloDev <catapiascastro@gmail.com>
2023-11-07 18:48:10 -0500

    -mfix(finished x12)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import threading
 import os
 
@@ -19,6 +20,13 @@ from src.Data.Macro.worker_macro import worker_fetch, get_data_dolar
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
 
 @app.get("/")
 def index():
